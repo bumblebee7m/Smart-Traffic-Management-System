122c61975faec9312be57bc43397a4290f4cac6e
OWUSU NANA KWARTENG <nanafx695@gmail.com>
2026-07-22 10:51:46 +0000

    Update Owusu_Nana_Kwarteng_smart_traffic_system.py

--- Owusu_Nana_Kwarteng_smart_traffic_system.py
+++ Owusu_Nana_Kwarteng_smart_traffic_system.py
@@ -1,29 +1,22 @@
 class TrafficDevice:
-    """Parent Class representing a generic traffic device."""
     def activate(self):
         print("Device activated.")
 
 class TrafficLight(TrafficDevice):
-    """Child Class representing a traffic signal light."""
     def activate(self):
         print("Traffic Light: Signals set to GREEN for smooth flow.")
 
 class SpeedCamera(TrafficDevice):
-    """Child Class representing a radar speed tracking camera."""
     def activate(self):
         print("Speed Camera: Monitoring vehicular speed and recording violations.")
 
 class PedestrianSignal(TrafficDevice):
-    """Child Class representing a crosswalk signal."""
     def activate(self):
         print("Pedestrian Signal: 'WALK' sign displayed for crosswalk access.")
 
-
 class EmergencySiren(TrafficDevice):
-    """Child Class representing an emergency warning siren."""
     def activate(self):
         print("Emergency Siren: BLARED! Emergency vehicle approaching clear lanes!")
-
 
 
 if __name__ == "__main__":
@@ -31,7 +24,7 @@ if __name__ == "__main__":
         TrafficLight(),
         SpeedCamera(),
         PedestrianSignal(),
-        EmergencySiren()  
+        EmergencySiren()
     ]
 
     print("--- Activating Smart City Traffic Devices ---")
